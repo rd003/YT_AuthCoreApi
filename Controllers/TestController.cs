9dd8ec90f65f63deb36e1db4a70a2882e25e9005
Ravindra Devrani <ravindra.devrani003@gmail.com>
2022-09-15 00:29:22 +0530

    logger added

---
+++ Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthCoreApi.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult TestDivideByZero()
+        {
+            try
+            {
+                int n = 12;
+                int sum = n / 0;
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Error on test controller, TestDivideByzeroMethod");
+            }
+            return Ok();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace AuthCoreApi
 {
@@ -13,11 +14,28 @@ namespace AuthCoreApi
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().
+                         AddJsonFile("appsettings.json").Build();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+            try
+            {
+                Log.Information("Application starting here");
+                 CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                 .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
